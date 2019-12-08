939f803d1353f753f8d0735ebe1567d299346699
Elise G <elisegrizzell@gmail.com>
2019-12-07 22:03:02 -0600

    Added theme

---
+++ lib/common/theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+final appTheme = ThemeData(
+  primarySwatch: Colors.yellow,
+  textTheme: TextTheme(
+    title: TextStyle(
+      fontFamily: 'Cinzel Decorative',
+      fontSize: 24,
+    ),
+    button: TextStyle(
+      fontFamily: 'Tangerine',
+      fontStyle: FontStyle.normal,
+      fontSize: 16,
+    ),
+    
+  ),
+);
