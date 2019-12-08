b524c0dbd26c9de75dac0e3874a55517a9cb3e23
Elise G <elisegrizzell@gmail.com>
2019-12-07 21:03:14 -0600

    login

--- lib/screens/loginscreen.dart
+++ lib/screens/loginscreen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class LoginScreen extends StatefulWidget {
   @override
   _LoginScreenState createState() => _LoginScreenState();
@@ -13,23 +12,29 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Info")), 
-        body: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
+      appBar: AppBar(title: Text("Info")),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              TextField(controller: emailCtrl),
+              // (DecoratedBox
+
+              //   child: TextField(controller: emailCtrl),
+              // ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(controller: emailCtrl),
+              ),
               TextField(controller: phoneCtrl, obscureText: true),
-              RaisedButton(child: Text("Go"), 
-              shape: CircleBorder(),
-              color: Color.fromARGB(0xff, 8, 96, 95),
-              onPressed: () {
-                print("Success");
-              }),
-            ]
-        ),
+              RaisedButton(
+                  child: Text("Go"),
+                  shape: CircleBorder(),
+                  color: Color.fromARGB(0xff, 8, 96, 95),
+                  onPressed: () {
+                    print("Success");
+                  }),
+            ]),
       ),
     );
   }
