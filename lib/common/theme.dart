824dd15f7800c5bdfa275cc9f16c29d4da471c06
Elise G <elisegrizzell@gmail.com>
2019-12-07 23:03:35 -0600

    Updated theme with button &  primary/sec colors

--- lib/common/theme.dart
+++ lib/common/theme.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 final appTheme = ThemeData(
-  primarySwatch: Colors.yellow,
+  primaryColor: treeGreen,
+  buttonColor: santaRed,
+  accentColor: darkGrey,
   textTheme: TextTheme(
     title: TextStyle(
       fontFamily: 'Cinzel Decorative',
@@ -15,3 +17,7 @@ final appTheme = ThemeData(
     
   ),
 );
+
+Color santaRed = const Color(0xa31621); 
+Color treeGreen = const Color(0x08605f); 
+Color darkGrey = const Color(0x2f4550); 
